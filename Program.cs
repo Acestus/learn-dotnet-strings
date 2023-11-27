91f42a1fdad5df4af12f5d8baad7693e4f5f6fdc
wbalconi <wbalconi@3arrows.us>
2023-11-26 19:55:03 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Enter numbers separated by hyphen: ");
+var input = Console.ReadLine().Split('-').Select(int.Parse).ToArray();
+Console.WriteLine(Consecutive(input));
+
+string Consecutive(int[] numbers)
+{
+    for (var i = 0; i < numbers.Length - 1; i++)
+    {
+        if (numbers[i] + 1 != numbers[i + 1])
+        {
+            return "Not Consecutive";
+        }
+    }
+    return "Consecutive";
+}
